fe7094ca46ad083d87a7d511ccdd789b338cf57c
gayu <sreeka496@gmail.com>
2021-01-15 16:02:12 +0530

    helloo

---
+++ LibraryManagementSystem/src/main/java/com/cts/library/dao/LibraryDao.java
@@ -0,0 +1,41 @@
+package com.cts.library.dao;
+
+import java.util.Date;
+
+import com.cts.library.pojo.Book;
+import com.cts.library.pojo.Customer;
+
+public class LibraryDao {
+	
+	public void insertBook() {
+		
+	}
+	public void insertCustomer() {
+		
+	}
+	public void bookReservations(int bookId, int customerId) {
+		
+	}
+	private Customer checkValidCustomer(int customerId) {
+		return null;
+		
+	}
+	private Book checkValidBook(int bookId) {
+		return null;
+		
+	}
+	private Book updateBookStock() {
+		return null;
+		
+	}
+	public Book returnBooks(int bookId, int customerId) {
+		return null;
+		
+	}
+	public void listBooksReservedByCustomer(int customerId) {
+		
+	}
+	public void totalBooksIssued(Date currentDate) {
+		
+	}
+}
